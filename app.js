1c624beea1c858c9f35d0bcaad3f55adfe22268a
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-12-25 06:32:14 -0400

    setting new ports

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ localApp.listen(localPort, () => {
 	console.log('Server running at: ' + localPort);
 });
 
-const local2Port = 3400;
+const local2Port = 3500;
 const local2Folder = './public/panaFront';
 const local2Path = local2Folder + '/index.html';
 const local2App = localServer.init(local2Folder, local2Path, local2Port);
@@ -31,10 +31,10 @@ const landingFolder = './public/panaLand';
 const landingPath = landingFolder + '/index.html';
 const landingApp = landingServer.init(landingFolder, landingPort);
 
- landingApp.get('/', (req, res) => {
+landingApp.get('/', (req, res) => {
 	res.send('We are having some troubles, please come back in a while!');
 });
- 
+
 //Pointing to angular app
 landingApp.get('/*', (req, res) => {
 	var fileToSend = path.join(__dirname, landingPath);
